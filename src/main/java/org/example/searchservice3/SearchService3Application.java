a65e10c2078a90ae9d9f43bd64703af7d9e01f71
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-05-03 09:22:51 +0200

    initial commit

---
+++ src/main/java/org/example/searchservice3/SearchService3Application.java
@@ -0,0 +1,13 @@
+package org.example.searchservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchService3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchService3Application.class, args);
+    }
+
+}
