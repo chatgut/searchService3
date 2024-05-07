e1b52f13d7b4ade6d51d18c4a1c54adc36d21b22
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-05-07 14:09:21 +0200

    add get message from elasticsearch

--- src/main/java/org/example/searchservice3/controller/SearchController.java
+++ src/main/java/org/example/searchservice3/controller/SearchController.java
@@ -1,14 +1,31 @@
 package org.example.searchservice3.controller;
 
+
+import org.example.searchservice3.service.SearchService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SearchController {
 
+
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+
     @GetMapping("/search")
     public String search() {
         return "search";
+    }
+
+    @GetMapping("/message/{id}")
+    public String message(@PathVariable String id) {
+
+        return searchService.searchMessage(id);
     }
 
 }
--- src/main/java/org/example/searchservice3/service/SearchService.java
+++ src/main/java/org/example/searchservice3/service/SearchService.java
@@ -2,20 +2,24 @@ package org.example.searchservice3.service;
 
 import org.example.searchservice3.entities.MessageDocument;
 import org.example.searchservice3.repository.SearchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class SearchService {
 
-
+    @Autowired
+    ElasticsearchOperations elasticsearchOperations;
     private final SearchRepository searchRepository;
+
+    // could replace @Autowired with @Qualifier("elasticsearchOperations") ElasticsearchOperations elasticsearchOperations in the constructor
     public SearchService(SearchRepository searchRepository) {
         this.searchRepository = searchRepository;
     }
 
-//    public void indexMessage(String id, String fromUser, String toUser, String messageText, String date) {
-//        System.out.println("Indexing message: " + id + " " + fromUser + " " + toUser + " " + messageText + " " + date);
-//    }
 
     public void indexMessage(String id, String fromUser, String toUser, String messageText, String date) {
         MessageDocument document = new MessageDocument();
@@ -24,8 +28,20 @@ public class SearchService {
         document.setTo(toUser);
         document.setMessage(messageText);
         document.setDate(date);
+        //elasticsearchOperations.save(document);
         searchRepository.save(document);
-        //todo: save the document to elasticsearch. use the imperative config??
         System.out.println("Indexing message: " + id + " " + fromUser + " " + toUser + " " + messageText + " " + date);
+    }
+
+
+    public String searchMessage(String id) {
+
+        MessageDocument document = elasticsearchOperations.get(Objects.requireNonNull(elasticsearchOperations.convertId(id)), MessageDocument.class);
+
+        if (document != null) {
+            return document.getMessage();
+        } else {
+            return null;
+        }
     }
 }
