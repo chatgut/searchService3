30f3611da85ea3d4bf11624835debbbcf63d4b6b
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-05-22 21:53:03 +0200

    update deployments and services

--- src/main/java/org/example/searchservice3/entities/MessageDocument.java
+++ src/main/java/org/example/searchservice3/entities/MessageDocument.java
@@ -4,12 +4,10 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 @Data
